9eeea6d721a55d687facf1d528d532a39ab82c6a
Felipe Carlos <efelipecarlos@gmail.com>
2026-08-22 11:47:45 -0300

    tuning: review sits_tuning translation and representations

--- tests/test_tuning.py
+++ tests/test_tuning.py
@@ -22,21 +22,19 @@ import pytest
 from pysits.conversions.dsl.tuning import hparam
 from pysits.models.data.tuning import SITSTuningResults
 from pysits.sits.context import samples_modis_ndvi
-from pysits.sits.ml import sits_tempcnn
+from pysits.sits.ml import sits_rfor, sits_tempcnn
 from pysits.sits.tuning import sits_tuning, sits_tuning_hparams
 
 #
 # Hyper-parameters options available for tuning
 #
+TRIALS = 2
+
 CNN_LAYERS = [[128.0, 128.0, 128.0], [64.0, 64.0, 64.0]]
 CNN_KERNELS = [[3.0, 3.0, 3.0], [5.0, 5.0, 5.0]]
 BETAS = [[0.9, 0.999], [0.85, 0.99]]
 OPTIMIZERS = ["optim_adamw", "optim_adam"]
-
-#
-# Number of trials used in the tests
-#
-TRIALS = 2
+NUM_TREES = [50.0, 100.0]
 
 
 @pytest.fixture(scope="module")
@@ -54,6 +52,20 @@ def tuned_tempcnn() -> SITSTuningResults:
                 lr=hparam("loguniform", 10**-2, 10**-4),
                 betas=hparam("choice", *[tuple(x) for x in BETAS]),
             ),
+        ),
+        trials=TRIALS,
+        multicores=1,
+    )
+
+
+@pytest.fixture(scope="module")
+def tuned_rfor() -> SITSTuningResults:
+    """Tuning results of a ``sits_rfor`` model."""
+    return sits_tuning(
+        samples=samples_modis_ndvi,
+        ml_method=sits_rfor,
+        params=sits_tuning_hparams(
+            num_trees=hparam("choice", *NUM_TREES),
         ),
         trials=TRIALS,
         multicores=1,
@@ -81,9 +93,9 @@ def test_tuning_vector_hparams(tuned_tempcnn):
 
 def test_tuning_scalar_hparams(tuned_tempcnn):
     """Test tuning results of hyper-parameters defined as scalars."""
-    assert tuned_tempcnn.epochs == [[1.0]] * TRIALS
-    assert tuned_tempcnn.validation_split == [[0.2]] * TRIALS
-    assert tuned_tempcnn.verbose == [[False]] * TRIALS
+    assert tuned_tempcnn.epochs == [1.0] * TRIALS
+    assert tuned_tempcnn.validation_split == [0.2] * TRIALS
+    assert tuned_tempcnn.verbose == [False] * TRIALS
 
 
 def test_tuning_optimizer_hparams(tuned_tempcnn):
@@ -98,9 +110,32 @@ def test_tuning_optimizer_hparams(tuned_tempcnn):
         assert opt_hparams["betas"] in BETAS
 
 
-def test_tuning_html_representation(tuned_tempcnn):
-    """Test tuning HTML representation."""
-    html = tuned_tempcnn._repr_html_()
+def test_tuning_generic_hparams(tuned_rfor):
+    """Test tuning results of models with no hyper-parameter properties."""
+    assert len(tuned_rfor.num_trees) == TRIALS
 
+    for num_trees in tuned_rfor.num_trees:
+        assert num_trees in NUM_TREES
+
+    # Dict accessor and property accessor must be equivalent
+    assert tuned_rfor.hparams["num_trees"] == tuned_rfor.num_trees
+
+    # Hyper-parameters with no value defined are returned as ``None``
+    assert tuned_rfor.mtry == [None] * TRIALS
+
+    # Hyper-parameters not tuned are not available
+    with pytest.raises(AttributeError):
+        tuned_rfor.unknown_hparam
+
+
+def test_tuning_html_representation(tuned_tempcnn, tuned_rfor):
+    """Test tuning HTML representation."""
+    # tempcnn representation
+    html = tuned_tempcnn._repr_html_()
     for layers in tuned_tempcnn.cnn_layers:
         assert str(layers) in html
+
+    # rfor representation
+    html = tuned_rfor._repr_html_()
+    for num_trees in tuned_rfor.num_trees:
+        assert str(num_trees) in html
